2ef7fe31aecc82312e06893de38e90f3f0aa0d3b
Naraharisetti Saiteja <59619359+SAITEJA115604@users.noreply.github.com>
2024-01-07 12:56:08 +0530

    new commiit
    new programs

---
+++ division_multiplication_addition_substractions_using_functions.py
@@ -0,0 +1,18 @@
+c = int(input("enter the number:"))
+d = int(input("enter the number:"))
+def add(c,d):
+    sum = c + d
+    print(sum)
+def sub(c,d):
+    min = c - d
+    print(min)
+def mul(c,d):
+    pro = c * d
+    print(pro)
+def division(c,d):
+    div = c/d
+    print(div)
+add(c,d)
+sub(c,d)
+mul(c,d)
+division(c,d)
